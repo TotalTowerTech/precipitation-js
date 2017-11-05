d9c1d9d8812a71a30221bd3fb1ad9d22e16f5428
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-05 17:02:27 -0600

    Added gitignore, fixed the ban command.

--- modules/commands/ban.js
+++ modules/commands/ban.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
         if (!message.user.hasPermission("BAN_MEMBERS")) return message.reply(`**:no_entry_sign: ERROR:**: Insufficient permissions.`);
 
         let person = message.mentions.users.first;
-        if (person.length <= 0 || person == null) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
+        if (person.length <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
 
         person.ban(args[0]).then(member => {
             message.reply(`${member.user.username} has been banned with reason ${args[0]}.`)
@@ -15,7 +15,6 @@ module.exports.run = async (client, message, args) => {
         .setTitle("An error has occured.")
         .setDescription(error.toString())
     }
-
 }
 module.exports.help = {
     name: 'ban',
