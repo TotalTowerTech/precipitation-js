00a2bd8874f1117fabad0ba29a491238b5e76c27
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-08 16:39:09 -0600

    More fixing.

--- modules/commands/corrupt.js
+++ modules/commands/corrupt.js
@@ -1,12 +1,12 @@
 module.exports.run = async (client, message, args) => {
     const Discord = require('discord.js');
-    var phrase = args.join(" ");
+    var phrase = args.join(" ").replace("a", phrase).replace("b", phrase).replace("c", phrase).replace("d", phrase).replace("e", phrase).replace("f", phrase).replace("g", phrase).replace("h", phrase).replace("i", phrase).replace("j", phrase).replace("k", phrase).replace("l", phrase).replace("m", phrase).replace("n", phrase).replace("o", phrase).replace("p", phrase).replace("q", phrase).replace("r", phrase).replace("s", phrase).replace("t", phrase).replace("u", phrase).replace("v", phrase).replace("w", phrase).replace("x", phrase).replace("y", phrase).replace("z", phrase);
 
-    phrase.replace("a", phrase).replace("b", phrase).replace("c", phrase).replace("d", phrase).replace("e", phrase).replace("f", phrase).replace("g", phrase).replace("h", phrase).replace("i", phrase).replace("j", phrase).replace("k", phrase).replace("l", phrase).replace("m", phrase).replace("n", phrase).replace("o", phrase).replace("p", phrase).replace("q", phrase).replace("r", phrase).replace("s", phrase).replace("t", phrase).replace("u", phrase).replace("v", phrase).replace("w", phrase).replace("x", phrase).replace("y", phrase).replace("z", phrase);
+    phrase
     let embed = new Discord.RichEmbed()
         .setAuthor("Corruption")
         .setDescription("Here's the corruption!")
-        .addField(":inbox_tray: Input:", `\`\`\`\n${args[0]}\n\`\`\``)
+        .addField(":inbox_tray: Input:", `\`\`\`\n${args.join(" ")}\n\`\`\``)
         .addField(":outbox_tray: Output:", `\`\`\`js\n${phrase}\n\`\`\``)
         .setColor("GREEN")
         .setFooter("Precipitation 0.1.2", client.user.avatarURL)
