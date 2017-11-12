cc8298e6d73a3d455232c3c808f3bf4ffa299f7a
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-12 13:08:09 -0600

    fixing...

--- modules/commands/getavatar.js
+++ modules/commands/getavatar.js
@@ -2,23 +2,28 @@ module.exports.run = async (client, message, args) => {
     const Discord = require('discord.js');
     let embed;
     let pingedPerson = message.mentions.users.first;
-    if (pingedPerson == null || pingedPerson.length == 0) {
-        embed = new Discord.RichEmbed()
-            .setTitle(`${message.author.username}'s profile picture! c:`)
-            .setDescription(message.author.displayAvatarURL)
-            .setFooter(`Requested by ${message.author.tag}`)
-            .setTimestamp(new Date())
-            .setColor("GREEN")
-        message.channel.send({ embed });
-    } else {
-        embed = new Discord.RichEmbed()
-            .setTitle(`${pingedPerson.username}'s profile picture! c:`)
-            .setDescription(pingedPerson.displayAvatarURL)
-            .setFooter(`Requested by ${pingedPerson.tag}`)
-            .setTimestamp(new Date())
-            .setColor("GREEN")
-        message.channel.send({ embed });
+    try {
+        if (pingedPerson == null || pingedPerson.length == 0) {
+            embed = new Discord.RichEmbed()
+                .setTitle(`${message.author.username}'s profile picture! c:`)
+                .setDescription(message.author.displayAvatarURL)
+                .setFooter(`Requested by ${message.author.tag}`)
+                .setTimestamp(new Date())
+                .setColor("GREEN")
+            message.channel.send({ embed });
+        } else {
+            embed = new Discord.RichEmbed()
+                .setTitle(`${pingedPerson.username}'s profile picture! c:`)
+                .setDescription(pingedPerson.displayAvatarURL)
+                .setFooter(`Requested by ${pingedPerson.tag}`)
+                .setTimestamp(new Date())
+                .setColor("GREEN")
+            message.channel.send({ embed });
+        }
+    } catch (e) {
+        message.channel.send(e);
     }
+    
 }
 module.exports.help = {
     name: 'getavatar',
