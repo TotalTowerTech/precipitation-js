acc85055644058afa75967444d3ffd2cb26896d0
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-12 14:38:13 -0600

    Thanks kaiss :P

--- modules/commands/getavatar.js
+++ modules/commands/getavatar.js
@@ -1,29 +1,27 @@
 module.exports.run = async (client, message, args) => {
 const Discord = require('discord.js');
  if (message.mentions.users.first()) {
-            	var mentionmembers = message.mentions.members.first()
-            	var mentionusers = message.mentions.users.first()
-            	var embed = new Discord.RichEmbed()
-            		.setAuthor("Avatar")
-            		.setDescription(`${mentionusers.username}'s current avatar`)
+            	var mentionMembers = message.mentions.members.first()
+            	var mentionUsers = message.mentions.users.first()
+                var embed = new Discord.RichEmbed()
+                    .setAuthor("Avatar")
+                    .setDescription(`${mentionusers.username}'s current avatar! c:`)
                     .setImage(mentionusers.displayAvatarURL)
                     .setColor("GREEN")
-            		.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL)
-            		.setTimestamp()
+                    .setFooter(`Requested by ${message.author.tag} on ${new Date()} | PrecipitationJS`, message.author.displayAvatarURL)
             	message.channel.send({embed})
             } else {
                 var embed = new Discord.RichEmbed()
                     .setAuthor("Avatar")
-                    .setDescription("Your current avatar")
+                    .setDescription("Your current avatar! c:")
                     .setImage(message.author.displayAvatarURL)
                     .setColor("GREEN")
-                    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL)
-                    .setTimestamp()
+                    .setFooter(`Requested by ${message.author.tag} on ${new Date()} | PrecipitationJS`, message.author.displayAvatarURL)
                 message.channel.send({embed})
             }
 }
 module.exports.help = {
     name: 'getavatar',
-    args: '[pingedPerson]',
+    args: '[mentionMembers] / [mentionUsers]',
     notes: 'Gets an avatar of a user.'
 }
