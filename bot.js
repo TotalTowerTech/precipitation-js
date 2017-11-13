2db20f725eea6d320c83acd50d4b51684e70069e
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-12 21:39:15 -0600

    rid bot.js of Rylan.

--- bot.js
+++ bot.js
@@ -1,21 +1,12 @@
 /****************************************
  * 
  *   Precipitation: Moderation bot, with a few fun commands.
- *   Copyright (C) 2017 Victor Tran and Rylan Arbour
- *   Rewrtitten by Rain.
+ *   Rewrtitten by Rain and FloppyDiskDrive.
  *
  *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *   it under the terms of the MIT License.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   
  * 
  * *************************************/
 
