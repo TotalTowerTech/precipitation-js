488cc58e283ea4c61e176f80ffd51fcf136b4e4f
jtsshieh <jtsshieh@outlook.com>
2017-11-13 17:28:06 -0500

    Update ban.js

--- modules/commands/ban.js
+++ modules/commands/ban.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
         if (!client.user.hasPermission("BAN_MEMBERS")) return message.reply("I do not have permission to ban.");
         if (!message.user.hasPermission("BAN_MEMBERS")) return message.reply(`**:no_entry_sign: ERROR:**: Insufficient permissions.`);
 
-        let person = message.mentions.users.first();
+        let person = message.mentions.members.first();
         if (person.length <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
 
         person.ban(args.join(" ")).then(member => {
