8e0adf9107637b6e9367da60c80acc1748b8fa9a
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-12 19:44:25 -0600

    yay debugging

--- modules/commands/guildinfo.js
+++ modules/commands/guildinfo.js
@@ -1,11 +1,16 @@
 module.exports.run = async (client, message, args) => {
     const Discord = require('discord.js');
-    let embed = new Discord.RichEmbed()
-        .setAuthor(`Info about ${message.guild.name}:`)
-        .addField(`Name: ${message.guild.name}\r\nServer Owner: ${message.guild.owner.user.username}\r\nRoles: ${message.guild.roles}\r\nNumber of Roles: ${message.guild.roles.size}\r\nNumber of Members: ${message.guilds.members.size}`)
-        .setColor("GREEN")
-        .setFooter(`Requested by ${message.author.tag} on ${new Date()} | PrecipitationJS v0.1.2`)
-    message.channel.send({embed})
+    try {
+        let embed = new Discord.RichEmbed()
+            .setAuthor(`Info about ${message.guild.name}:`)
+            .addField(`Name: ${message.guild.name}\r\nServer Owner: ${message.guild.owner.user.username}\r\nRoles: ${message.guild.roles}\r\nNumber of Roles: ${message.guild.roles.size}\r\nNumber of Members: ${message.guilds.members.size}`)
+            .setColor("GREEN")
+            .setFooter(`Requested by ${message.author.tag} on ${new Date()} | PrecipitationJS v0.1.2`)
+        message.channel.send({ embed })
+    } catch (e) {
+        message.channel.send("```" + e + "```");
+    }
+    
 }
 module.exports.help = {
     name: 'guildinfo',
