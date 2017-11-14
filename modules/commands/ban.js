eb4a5cfe7b2074dcefa8eb7a717cf7a58a7220b4
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-14 17:45:40 -0600

    Revert "more enchancements yay"
    This reverts commit c321785e8af1fab885378c066d63ac18cd9fc9c3.

--- modules/commands/ban.js
+++ modules/commands/ban.js
@@ -4,11 +4,11 @@ module.exports.run = async (client, message, args, throwex) => {
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`**:no_entry_sign: ERROR:**: Insufficient permissions.`);
 
         let person = message.guild.member(message.mentions.users.first());
-        if (person.length <= 0 || person == null || person.size <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
+        if (person.length <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
 
         message.guild.member(person).ban(args.join(" ")).then(member => {
-            message.reply(`${member.user.username} has been banned with reason ${args.join(" ").substr(person.length - 1)}.`)
-            member.send(`You have been banned from **${message.guild.name}** with the following reason: \r\n\`\`\`${args.join(" ")(person.length - 1)}\`\`\`\`\n Goodbye, and we hope to never see you again.`)
+            message.reply(`${member.user.username} has been banned with reason ${args.join(" ")}.`)
+            member.send(`You have been banned from **${message.guild.name}** with the following reason: \r\n\`\`\`${args.join(" ")}\`\`\`\`\n Goodbye, and we hope to never see you again.`)
         })
     } catch (error) {
         throwex(error);
--- modules/commands/kick.js
+++ modules/commands/kick.js
@@ -7,8 +7,8 @@ module.exports.run = async (client, message, args, throwex) => {
         if (person.length <= 0 || person == null || person.size <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
 
         message.guild.member(person).kick(args.join(" ")).then(member => {
-            message.reply(`${member.user.username} has been kicked with reason ${args.join(" ").substr(person.length - 1)}.`)
-            member.send(`You have been kicked from **${client.guild.name}** with the following reason: \r\n\`\`\`${args.join(" ").substr(person.length - 1)}\`\`\`\`\n You may be able to join, but it's best to wait a few days to set back a bit.`)
+            message.reply(`${member.user.username} has been kicked with reason ${args.join(" ")}.`)
+            member.send(`You have been kicked from **${client.guild.name}** with the following reason: \r\n\`\`\`${args.join(" ")}\`\`\`\`\n You may be able to join, but it's best to wait a few days to set back a bit.`)
         })
     } catch (error) {
         throwex(error);
