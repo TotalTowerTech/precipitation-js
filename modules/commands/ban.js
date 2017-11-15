c3597e5bc81a0bcf8ef8a454e632eb11276539b7
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-15 17:05:12 -0600

    More changes

--- modules/commands/ban.js
+++ modules/commands/ban.js
@@ -1,14 +1,15 @@
 module.exports.run = async (client, message, args, throwex) => {
     const Discord = require('discord.js')
+    
     try {
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`**:no_entry_sign: ERROR:**: Insufficient permissions.`);
 
         let person = message.guild.member(message.mentions.users.first());
-        if (person.length <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
-
-        message.guild.member(person).ban(args.join(" ")).then(member => {
+        if (person.size <= 0) return message.reply("**:no_entry_sign: ERROR:** No one was pinged.");
+        let reason = args.join(" ").substr(person.size - 1);
+        message.guild.member(person).ban(reason).then(member => {
             message.reply(`${member.user.username} has been banned with reason ${args.join(" ")}.`)
-            member.send(`You have been banned from **${message.guild.name}** with the following reason: \r\n\`\`\`${args.join(" ")}\`\`\`\`\n Goodbye, and we hope to never see you again.`)
+            member.send(`You have been banned from **${message.guild.name}** with the following reason: \r\n\`\`\`${reason}\`\`\`\`\r\n Goodbye, and we hope to never see you again.`)
         })
     } catch (error) {
         throwex(error);
