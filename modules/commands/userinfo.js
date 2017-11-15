e41b7e3b1267b512f02efc3ea744001594af6972
jtsshieh <jtsshieh@outlook.com>
2017-11-14 22:47:28 -0500

    Update userinfo.js

--- modules/commands/userinfo.js
+++ modules/commands/userinfo.js
@@ -7,14 +7,14 @@ module.exports.run = async (client, message, args, throwex) => {
                 .setTitle(`Information about ${message.author.username}:`)
                 .addField("Account Information ", `Username: ${message.author.username}\r\nUser Discriminator: ${message.author.discriminator}\r\nUser ID: ${message.author.id}`)
                 .addField("Dates", `Creation Date: ${message.author.createdAt}\r\nJoined Date: ${message.member.joinedAt}`)
-                .addField("Presence", `${message.author.presence.game.name}`)
+                .addField("Presence", `${message.author.presence}`)
             message.channel.send({ embed })
         } else {
             var embed = new Discord.RichEmbed()
                 .setTitle(`Information about ${pinged.user.username}:`)
                 .addField("Account Information ", `Username: ${pinged.user.username}\r\nUser Discriminator: ${pinged.user.discriminator}\r\nUser ID: ${pinged.user.id}`)
                 .addField("Dates", `Creation Date: ${pinged.user.createdAt}\r\nJoined Date: ${pinged.joinedAt}`)
-                .addField("Presence", `${pinged.user.presence.game.name}`)
+                .addField("Presence", `${pinged.user.presence}`)
             message.channel.send({ embed })
         }
         } catch (e) {
