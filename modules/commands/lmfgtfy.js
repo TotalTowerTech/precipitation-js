82d1e4e12e5f652c51f7660e095b6bb4f438f1c7
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-15 20:02:26 -0600

    more things

--- modules/commands/lmfgtfy.js
+++ modules/commands/lmfgtfy.js
@@ -1,11 +1,10 @@
 module.exports.run = async (client, message, args, throwex) => {
     const Discord = require('discord.js');
-    let searchResult = args.join(" ").toLowerCase();
-    searchResult.replace(" ", "+");
+    
     try {
         let embed = new Discord.RichEmbed()
             .setTitle(`LMFGTFY - ${args.join(" ")}`)
-            .addField(`www.lmfgtfy.com/${searchResult}`)
+            .setDescription(`www.lmfgtfy.com/${args.join(" ").toLowerCase().replace(" ", "+")}`)
             .setFooter(`Requested by ${message.author.username} on ${new Date()} | PrecipitationJS v0.1.2`)
         message.channel.send({ embed });
     } catch (e) {
@@ -14,7 +13,7 @@ module.exports.run = async (client, message, args, throwex) => {
 }
 module.exports.help = {
     name: 'lmfgtfy',
-    args: '[searchResult]',
+    args: '[args]',
     notes: 'Be the biggest smartaleck on the guild and link them to this if they have a question.',
     category: 'Fun'
 }
