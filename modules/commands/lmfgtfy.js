ee5eab76d6e8b40a230fad0c7428a017dc13a1ad
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-15 20:19:41 -0600

    trying again weeeee

--- modules/commands/lmfgtfy.js
+++ modules/commands/lmfgtfy.js
@@ -1,10 +1,14 @@
 module.exports.run = async (client, message, args, throwex) => {
     const Discord = require('discord.js');
+    function replaceAll(search, placement) {
+        var target = this;
+        return target.replace(new RegExp(search, 'g'), replacement)
+    }
     
     try {
         let embed = new Discord.RichEmbed()
             .setTitle(`LMFGTFY - ${args.join(" ")}`)
-            .setDescription(`www.lmfgtfy.com/${args.join(" ").toLowerCase().replace(" ", "+")}`)
+            .setDescription(`www.lmfgtfy.com/${args.join(" ").replaceAll(" "), "+"}`)
             .setFooter(`Requested by ${message.author.username} on ${new Date()} | PrecipitationJS v0.1.2`)
         message.channel.send({ embed });
     } catch (e) {
