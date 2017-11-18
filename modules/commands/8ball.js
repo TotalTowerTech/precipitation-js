730c5858c353cf0f08204c391f0f278812332269
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-17 19:50:42 -0600

    dingi

--- modules/commands/8ball.js
+++ modules/commands/8ball.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args, throwex) => {
     let replies = ["The future is very shaky with this question.", "Ask again later.", "Most definitely.", "The future is not in your favor.", "Most likely.", "Yes.", "No."];
     if (!args.join(" ")) return message.reply("You must enter a question.");
-    message.reply(replies[Math.random() * replies.length]);
+    message.reply(replies[Math.floor(Math.random() * replies.length)]);
 }
 module.exports.help = {
     name: '8ball',
