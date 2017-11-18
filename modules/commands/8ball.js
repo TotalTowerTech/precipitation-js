961846f02b8ea240b14fe731b1e0a3c79e418be0
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-17 19:39:58 -0600

    maybe this is the reason

--- modules/commands/8ball.js
+++ modules/commands/8ball.js
@@ -1,4 +1,4 @@
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, throwex) => {
     let replies = ["The future is very shaky with this question.", "Ask again later.", "Most definitely.", "The future is not in your favor.", "Most likely.", "Yes.", "No."];
     if (!args.join(" ")) return message.reply("You must enter a question.");
     message.reply(replies[Math.random() * replies.length]);
