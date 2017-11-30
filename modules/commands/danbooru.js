8f4e5dfca43d203958a9e917beb2553cd7d7dff9
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-30 06:50:59 -0600

    maybe this will work

--- modules/commands/danbooru.js
+++ modules/commands/danbooru.js
@@ -1,39 +1,32 @@
 module.exports.run = async (client, message, args, throwex) => {
     const Discord = require('discord.js');
-    const Kaori = require('kaori');
-    var koari = new Koari();
+    var Booru = require('booru');
+    var booru = new Booru();
+    
     var rndTags = ['furry', 'gay', 'incest', 'hentai', 'oppai', 'cat', 'pokemon'];
     const titles = ["Here's that stuff for ya~", "This isn't even NSFW for you?", "This has a *lot* more than just countries it shows..."];
     if (message.channel.nsfw) {
         try {
-            if (!args[0]) {
-                kaori.search('danbooru', {
-                    tags: [rndTags[Math.floor(Math.random() * rndTags.length)]],
-                    limit: 1,
-                    random: true
-                }).then(images => {
-                    let embed = new Discord.RichEmbed()
-                        .setTitle(titles[Math.floor(Math.random() * titles.length)])
-                        .setImage(images[0].common.fileURL())
-                        .setColor('GREEN')
-                    message.channel.send({ embed });
+            booru.search('danbooru', [args[0], args[1]], { limit: 1, random: true })
+                .then(booru.commonfy)
+                .then(images => {
+                    //Log the direct link to each image 
+                    for (let image of images) {
+                        let e = new Discord.RichEmbed()
+                            .setImage(image.common.file_url)
+                            .setTitle(titles[Math.floor(Math.random() * titles.length)])
+                            .setColor('GREEN')
+                        message.channel.send({ e });
+                    }
                 })
-            }
-            kaori.search('danbooru', {
-                tags: [args[0]],
-                limit: 1,
-                random: true
-            }).then(images => {
-                let embed = new Discord.RichEmbed()
-                    .setTitle(titles[Math.floor(Math.random() * titles.length)])
-                    .setImage(images[0].common.fileURL())
-                    .setColor('GREEN')
-                message.channel.send({ embed });
-            })
-        } catch (e) {
-            throwex(e);
+                .catch(err => {
+                    if (err.name === 'booruError') {
+                        throwex(err.name);
+                    } else {
+                        throwex(err);
+                    }
+                }) 
         }
-        
     } else {
         message.channel.send("This channel is not marked as an NSFW channel. Please try again with a properly set up NSFW channel.");
         return;
