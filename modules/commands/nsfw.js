7ff1d7cc2cc9752ffd54e7a6b640cb1a16f933bd
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-11-29 21:17:46 -0600

    woo debug

--- modules/commands/nsfw.js
+++ modules/commands/nsfw.js
@@ -4,17 +4,25 @@ module.exports.run = async (client, message, args, throwex) => {
     const kaori = require('kaori');
     const titles = ["Here's that stuff for ya~", "This isn't even NSFW for you?", "This has a *lot* more than just countries it shows..."];
     if (message.channel.nsfw) {
-        kaori.search('e621', {
-            tags: [args[0]],
-            limit: 1,
-            random: true
-        }).then(images => {
-            let embed = new Discord.RichEmbed()
-                .setTitle(titles[Math.floor(Math.random() * titles.length)])
-                .setImage(images[0].common.fileURL())
-                .setColor('GREEN')
-            message.channel.send({ embed });
-        })
+        try {
+            kaori.search('e621', {
+                tags: [args[0]],
+                limit: 1,
+                random: true
+            }).then(images => {
+                let embed = new Discord.RichEmbed()
+                    .setTitle(titles[Math.floor(Math.random() * titles.length)])
+                    .setImage(images[0].common.fileURL())
+                    .setColor('GREEN')
+                message.channel.send({ embed });
+            })
+        } catch (e) {
+            throwex(e);
+        }
+        
+    } else {
+        message.channel.send("This channel is not marked as an NSFW channel. Please try again with a properly set up NSFW channel.");
+        return;
     }
 }
 
