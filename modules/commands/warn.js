9db0ccee720b7976d9d4d3f9679bed96ec774f83
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-02 22:24:39 -0600

    IM RETRARDED

--- modules/commands/warn.js
+++ modules/commands/warn.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args, throwex) => {
         time: 15000,
         errors: ['time']
     }).then(collected => {
-        message.channel.send(collected.content);
+        message.channel.send(collected.first().content);
         }).catch(() => {
             message.channel.send("No reason was specified. Cancelling action.");
         })
