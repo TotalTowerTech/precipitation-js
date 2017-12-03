20d19611e31f5d10d76adb2d38967007e7adf9bb
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-02 22:19:49 -0600

    woo

--- modules/commands/warn.js
+++ modules/commands/warn.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args, throwex) => {
         time: 15000,
         errors: ['time']
     }).then(collected => {
-        message.channel.send(message.content);
+        message.channel.send(collected.content);
         }).catch(() => {
             message.channel.send("No reason was specified. Cancelling action.");
         })
