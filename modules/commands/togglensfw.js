0b0b7bf2bc8057069331c3e49344b5b0e793b889
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-02 21:53:07 -0600

    woo let's try

--- modules/commands/togglensfw.js
+++ modules/commands/togglensfw.js
@@ -9,7 +9,7 @@ module.exports.run = async (message, client, args, throwex) => {
                     fs.writeFile('.../config.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) { if (err) throwex(err); })
                     message.channel.send('Okay, I disabled NSFW for now. Reenable this by running the command again!');
                 } else {
-                    arrayOfObjects[message.guild.name]['toggleNSFW'] == false;
+                    arrayOfObjects[message.guild.name]['toggleNSFW'] == true;
                     fs.writeFile('.../config.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) { if (err) throwex(err); })
                     message.channel.send('Okay, I reenabled NSFW for now. Redisable this by running the command again!');
                 }
--- modules/commands/warn.js
+++ modules/commands/warn.js
@@ -4,18 +4,14 @@ module.exports.run = async (client, message, args, throwex) => {
     let foundUser = message.mentions.users.first();
     message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
         .then(() => {
-            message.channel.awaitMessages(reason => reason.content == message.content, {
-    max: 1,
-    time: 30000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.channel.send("lol how do i warn");
-    })
-    .catch(() => {
-      message.channel.send('There was no collected message that passed the filter within the time limit!');
-    });
-});
+            const filter = m => m.author == message.author;
+            message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+                .then(collected => {
+                    message.channel.send("lol how do i warn");
+                })
+                .catch(() => {
+                    message.channel.send('There was no collected message that passed the filter within the time limit!');
+                });
 }
 module.exports.help = {
     name: 'warn',
