e204a82e700e055a1215ce1c851266c2d57b58b2
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-02 22:04:18 -0600

    woo

--- modules/commands/warn.js
+++ modules/commands/warn.js
@@ -1,18 +1,20 @@
 module.exports.run = async (client, message, args, throwex) => {
     let warnCount = 0;
     const fs = require('fs');
+    const filter = m => m.author == message.author;
     let foundUser = message.mentions.users.first();
-    message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
-        .then(() => {
-            const filter = m => m.author == message.author;
-            message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
-                .then(collected => {
-                    message.channel.send("lol how do i warn");
-                })
-                .catch(() => {
-                    message.channel.send('There was no collected message that passed the filter within the time limit!');
-                });
-}
+    message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.');
+    message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 15000,
+        errors: ['time']
+    }).then(collected => {
+        message.channel.send("lol how do i warn");
+        }).catch(() => {
+            message.channel.send("No reason was specified. Cancelling action.");
+        })
+    }
+
 module.exports.help = {
     name: 'warn',
     args: '[userToFind]',
