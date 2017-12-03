e7beb4342e1ccb19bb406c1d0fc133960e4e723b
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-02 20:33:10 -0600

    pls work

--- modules/commands/warn.js
+++ modules/commands/warn.js
@@ -3,14 +3,14 @@ module.exports.run = async (client, message, args, throwex) => {
     const fs = require('fs');
     let foundUser = message.member.mentions.first();
     message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
-.then(() => {
-  message.channel.awaitMessages(reason => {
+        .then(() => {
+            message.channel.awaitMessages(reason => reason.content == message.content, {
     max: 1,
     time: 30000,
     errors: ['time'],
   })
   .then((collected) => {
-      message.channel.send(`The collected message was: ${collected.first().content}`);
+      message.channel.send("lol how do i warn");
     })
     .catch(() => {
       message.channel.send('There was no collected message that passed the filter within the time limit!');
