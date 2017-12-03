2e3bf93bf567bead12a0c36e309cbc1d061854f7
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-12-03 15:53:52 -0600

    im bad at codding

--- modules/commands/ping.js
+++ modules/commands/ping.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args) => {
     var pingReplies = ["I'm totally not AstralMod!", "Sausages.", "Rain, rain, go away...", "Don't do drugs, kids.", "[insert generic response here]"];
     const e = await message.channel.send('Pinging...');
-    return e.edit(`:ping_pong: Pong! ${pingReplies[Math.floor(Math.random() * pingReplies.length)]} The client's ping is ${Math.round(client.ping)} ms. The API ping is ${Math.round(e.createdTimestamp - message.editedTimestamp)} ms.`)
+    return e.edit(`:ping_pong: Pong! ${pingReplies[Math.floor(Math.random() * pingReplies.length)]} The client's ping is ${Math.round(client.ping)} ms. The API ping is ${Math.round(e.createdTimestamp - message.createdTimestamp)} ms.`)
 }
 
 module.exports.help = {
